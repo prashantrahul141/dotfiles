2afb5837ad6fb741278517874e1d504512db626e
prashantrahul141 <prashantrahul141@protonmail.com>
2025-02-19 10:25:30 +0530

    up

--- modules/hyprland/hyprland.nix
+++ modules/hyprland/hyprland.nix
@@ -210,8 +210,8 @@
       ",XF86AudioLowerVolume, exec, wpctl set-volume @DEFAULT_AUDIO_SINK@ 5%-"
       ",XF86AudioMute, exec, wpctl set-mute @DEFAULT_AUDIO_SINK@ toggle"
       ",XF86AudioMicMute, exec, wpctl set-mute @DEFAULT_AUDIO_SOURCE@ toggle"
-      ",XF86MonBrightnessUp, exec, brightnessctl s 10%+"
-      ",XF86MonBrightnessDown, exec, brightnessctl s 10%-"
+      ",XF86MonBrightnessUp, exec, brightnessctl s 5%+"
+      ",XF86MonBrightnessDown, exec, brightnessctl s 5%-"
     ];
 
     # Requires playerctl
