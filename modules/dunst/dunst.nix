3f1f919d557a2ab9a7166e2a8b99f3d64ed09666
prashantrahul141 <prashantrahul141@protonmail.com>
2025-02-20 14:59:41 +0530

    up

--- modules/dunst/dunst.nix
+++ modules/dunst/dunst.nix
@@ -4,35 +4,43 @@
 
   services.dunst.settings = {
     global = {
-      mouse_left_click = "close_current";
-      mouse_right_click = "do_action";
-      mouse_middle_click = "close_all";
+      mouse_left_click = "do_action";
+      mouse_right_click = "context";
+      mouse_middle_click = "close_current";
       frame_color = "#${config.colorScheme.palette.base03}";
       separator_color = "#${config.colorScheme.palette.base03}";
-      font = "FiraCode Nerd Font 10";
+      font = "FiraCode Nerd Font 11";
       shrink = true;
       word_wrap = "yes";
+      offset = "9x9";
+      padding =  15;
+      horizontal_padding = 15;
+      dmenu = " wofi -d -p dunst";
+      corner_radius = 10;
+      format = "<b>%s</b>\n%b";
+      sticky_history = "no";
     };
 
     base16_low = {
       msg_urgency = "low";
       background = "#${config.colorScheme.palette.base00}";
       foreground = "#${config.colorScheme.palette.base07}";
-      timeout = 5;
+      timeout = 3;
     };
 
     base16_normal = {
       msg_urgency = "normal";
       background = "#${config.colorScheme.palette.base00}";
       foreground = "#${config.colorScheme.palette.base07}";
-      timeout = 5;
+      timeout = 3;
     };
 
     base16_critical = {
       msg_urgency = "critical";
       background = "#${config.colorScheme.palette.base00}";
       foreground = "#${config.colorScheme.palette.base08}";
-      timeout = 0;
+      frame_color = "#${config.colorScheme.palette.base08}";
+      timeout = 5;
     };
   };
 }
