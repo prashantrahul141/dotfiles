56c40166d0aecb035b4e3565de4ce4459a665384
prashantrahul141 <prashantrahul141@protonmail.com>
2025-03-16 22:38:24 +0530

    lmao

--- home/prashant/others.nix
+++ home/prashant/others.nix
@@ -1,13 +1,16 @@
 { ... }:
 {
-  xdg.mimeApps.defaultApplications = {
-    "text/plain" = [ "nvim.desktop" ];
-    "application/pdf" = [ "evince.desktop" ];
-    "image/*" = [ "loupe.desktop" ];
-    "video/png" = [ "mpv.desktop" ];
-    "video/jpg" = [ "mpv.desktop" ];
-    "video/*" = [ "mpv.desktop" ];
-    "inode/directory" = [ "nemo.desktop" ];
-    "application/x-shellscript" = [ "kitty.desktop" ];
+  xdg.mimeApps = {
+    enable = true;
+    defaultApplications = {
+      "text/plain" = [ "nvim.desktop" ];
+      "application/pdf" = [ "evince.desktop" ];
+      "image/*" = [ "nsxiv.desktop" ];
+      "video/png" = [ "mpv.desktop" ];
+      "video/jpg" = [ "mpv.desktop" ];
+      "video/*" = [ "mpv.desktop" ];
+      "inode/directory" = [ "nemo.desktop" ];
+      "application/x-shellscript" = [ "kitty.desktop" ];
+    };
   };
 }
