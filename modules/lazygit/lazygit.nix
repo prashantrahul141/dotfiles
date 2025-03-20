2e27c6dcae652fb94dd3c0c0c4d7b6f90e4f07da
prashantrahul141 <prashantrahul141@protonmail.com>
2025-03-20 10:18:01 +0530

    enable sign-off
    Signed-off-by: prashantrahul141 <prashantrahul141@protonmail.com>

--- modules/lazygit/lazygit.nix
+++ modules/lazygit/lazygit.nix
@@ -3,12 +3,17 @@
   programs.lazygit = {
     enable = true;
     settings = {
-     nerdFontsVersion = "2";
-     timeFormat = "02/01/2006";
-     shortTimeFormat =  "15:04:05";
-     pager = "bat";
-     externalDiffCommand = "batdiff";
-     disableStartupPopups = true;
+      git = {
+        commit = {
+          signOff = true;
+        };
+      };
+      nerdFontsVersion = "2";
+      timeFormat = "02/01/2006";
+      shortTimeFormat = "15:04:05";
+      pager = "bat";
+      externalDiffCommand = "batdiff";
+      disableStartupPopups = true;
     };
   };
 }
