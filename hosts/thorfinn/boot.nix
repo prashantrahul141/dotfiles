ed528c3dd988796c86e95c63430b40b9ff75ffc2
prashantrahul141 <prashantrahul141@protonmail.com>
2025-05-24 11:57:45 +0530

    fix: ignore power key
    Signed-off-by: prashantrahul141 <prashantrahul141@protonmail.com>

--- hosts/thorfinn/boot.nix
+++ hosts/thorfinn/boot.nix
@@ -4,5 +4,10 @@
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
 
+  # don’t shutdown when power button is short-pressed
+  services.logind.extraConfig = ''
+    HandlePowerKey=ignore
+  '';
+
   system.stateVersion = "24.11";
 }
