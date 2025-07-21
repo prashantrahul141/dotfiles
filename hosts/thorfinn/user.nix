fe794048e1127c659d36a965ca495c2079fe2da3
prashantrahul141 <prashantrahul141@protonmail.com>
2025-07-21 15:32:45 +0530

    feat: add only default locale
    Signed-off-by: prashantrahul141 <prashantrahul141@protonmail.com>

--- hosts/thorfinn/user.nix
+++ hosts/thorfinn/user.nix
@@ -10,19 +10,7 @@
   time.timeZone = "Asia/Kolkata";
 
   # Select internationalisation properties.
-  i18n.defaultLocale = "en_IN";
-
-  i18n.extraLocaleSettings = {
-    LC_ADDRESS = "en_IN";
-    LC_IDENTIFICATION = "en_IN";
-    LC_MEASUREMENT = "en_IN";
-    LC_MONETARY = "en_IN";
-    LC_NAME = "en_IN";
-    LC_NUMERIC = "en_IN";
-    LC_PAPER = "en_IN";
-    LC_TELEPHONE = "en_IN";
-    LC_TIME = "en_IN";
-  };
+  i18n.defaultLocale = "en_US.UTF-8";
 
   # need this even if it was enabled in home manager, allows settings defaultUserShell
   programs.zsh.enable = true;
