cb12c0bc0718b45cd186225abce9d93e3630f3c8
prashantrahul141 <prashantrahul141@protonmail.com>
2025-12-01 04:02:23 +0530

    feat: fake window for marking window floaty
    Signed-off-by: prashantrahul141 <prashantrahul141@protonmail.com>

--- modules/hyprland/hyprland.nix
+++ modules/hyprland/hyprland.nix
@@ -297,6 +297,7 @@
       "float, class:nwg-*"
       "float, class:qemu"
       "float, class:swayimg"
+      "float, title:marked-floating"
 
       # Fix some dragging issues with XWayland
       "nofocus,class:^$,title:^$,xwayland:1,floating:1,fullscreen:0,pinned:0"
