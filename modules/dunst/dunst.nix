da8865e0142ecbf9a28d705e134ae32ec8e46036
prashantrahul141 <prashantrahul141@protonmail.com>
2025-12-08 00:25:54 +0530

    fix(dunst): revert old settings due to timeouts not working
    Signed-off-by: prashantrahul141 <prashantrahul141@protonmail.com>

--- modules/dunst/dunst.nix
+++ modules/dunst/dunst.nix
@@ -1,46 +1,48 @@
 { colorScheme, ... }:
 {
-  services.dunst.enable = true;
+  services.dunst = {
+    enable = true;
+    settings = {
+      global = {
+        mouse_left_click = "do_action";
+        mouse_right_click = "context";
+        mouse_middle_click = "close_current";
+        frame_color = "#${colorScheme.palette.base03}";
+        separator_color = "#${colorScheme.palette.base03}";
+        font = "FiraCode Nerd Font 11";
+        shrink = true;
+        word_wrap = "yes";
+        offset = "(9, 9)";
+        padding = 15;
+        horizontal_padding = 15;
+        dmenu = "wofi -d -p dunst";
+        corner_radius = 10;
+        format = "<b>%s</b>\\n%b";
+        sticky_history = "no";
+        monitor = 1;
+      };
 
-  services.dunst.settings = {
-    global = {
-      mouse_left_click = "do_action";
-      mouse_right_click = "context";
-      mouse_middle_click = "close_current";
-      frame_color = "#${colorScheme.palette.base03}";
-      separator_color = "#${colorScheme.palette.base03}";
-      font = "FiraCode Nerd Font 11";
-      shrink = true;
-      word_wrap = "yes";
-      offset = "(9, 9)";
-      padding = 15;
-      horizontal_padding = 15;
-      dmenu = "wofi -d -p dunst";
-      corner_radius = 10;
-      format = "<b>%s</b>\\n%b";
-      sticky_history = "no";
-      monitor = 1;
-      progress_bar = true;
+      base16_low = {
+        msg_urgency = "low";
+        background = "#${colorScheme.palette.base00}";
+        foreground = "#${colorScheme.palette.base07}";
+        timeout = 3000;
+      };
+
+      base16_normal = {
+        msg_urgency = "normal";
+        background = "#${colorScheme.palette.base00}";
+        foreground = "#${colorScheme.palette.base07}";
+        timeout = 3000;
+      };
+
+      base16_critical = {
+        msg_urgency = "critical";
+        background = "#${colorScheme.palette.base00}";
+        foreground = "#${colorScheme.palette.base08}";
+        frame_color = "#${colorScheme.palette.base08}";
+        timeout = 5000;
+      };
     };
-
-    urgency_low = {
-      background = "#${colorScheme.palette.base00}";
-      foreground = "#${colorScheme.palette.base07}";
-      timeout = 4000;
-    };
-
-    urgency_normal = {
-      background = "#${colorScheme.palette.base00}";
-      foreground = "#${colorScheme.palette.base07}";
-      timeout = 4000;
-    };
-
-    urgency_critical = {
-      background = "#${colorScheme.palette.base00}";
-      foreground = "#${colorScheme.palette.base08}";
-      frame_color = "#${colorScheme.palette.base08}";
-      timeout = 6000;
-    };
-
   };
 }
