842c5dabb9bd3924e23a98b22b235d3defa64dfd
Prashant Rahul <prashantrahul141@protonmail.com>
2026-02-08 20:06:32 +0530

    feat(nvim): folding binds
    Signed-off-by: Prashant Rahul <prashantrahul141@protonmail.com>

--- modules/neovim/default.nix
+++ modules/neovim/default.nix
@@ -29,6 +29,19 @@
       vim.keymap.set('v', '<RightMouse>', '<Nop>', { noremap = true })
 
       vim.o.shortmess = "atI"
+
+      -- for folding
+      vim.opt.foldmethod = "expr"
+
+      -- :h vim.treesitter.foldexpr()
+      vim.opt.foldexpr = "v:lua.vim.treesitter.foldexpr()"
+
+      -- ref: https://github.com/neovim/neovim/pull/20750
+      vim.opt.foldtext = ""
+      vim.opt.fillchars:append("fold: ")
+
+      -- Open all folds by default, zm is not available
+      vim.opt.foldlevelstart = 99
     '';
 
     extraConfigLuaPre = ''
