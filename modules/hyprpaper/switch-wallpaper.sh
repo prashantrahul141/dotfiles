81e47ea7cbaa5bd4e4bb0a171ab1795c4898dcf6
Prashant Rahul <prashantrahul141@protonmail.com>
2026-05-29 18:00:30 +0530

    fix(hyprpaper): better wallpaper switch script
    Signed-off-by: Prashant Rahul <prashantrahul141@protonmail.com>

--- modules/hyprpaper/switch-wallpaper.sh
+++ modules/hyprpaper/switch-wallpaper.sh
@@ -1,10 +1,26 @@
 #!/usr/bin/env bash
 
-WALLPAPER_DIR="$HOME/Pictures/Wallpapers/curr/"
+WALLPAPER_DIR="$HOME/Pictures/Wallpapers/curr"
 CURRENT_WALL=$(hyprctl hyprpaper listloaded)
 
-# Get a random wallpaper that is not the current one
-WALLPAPER=$(find "$WALLPAPER_DIR" -type f ! -name "$(basename "$CURRENT_WALL")" | shuf -n 1)
+# find index of current file, also count of all files.
+CURRENT_INDEX=0
+TOTAL_WALLS=0
+for file in "$WALLPAPER_DIR"/*; do
+    ((TOTAL_WALLS+=1))
+    if [[ "$file" == "$CURRENT_WALL" ]]; then
+        CURRENT_INDEX=$TOTAL_WALLS
+    fi
+done
 
-# Apply the selected wallpaper
-hyprctl hyprpaper reload ,"$WALLPAPER"
+# select next wallpaper and set
+SET_WALL_TO_INDEX=$(($((CURRENT_INDEX + 1)) % TOTAL_WALLS))
+COUNT=0
+for file in "$WALLPAPER_DIR"/*; do
+    if [[ "$COUNT" == "$SET_WALL_TO_INDEX" ]]; then
+        hyprctl hyprpaper reload ", $file"
+        break
+    fi
+
+    ((COUNT+=1))
+done
